750750ae9a6fe6665ab843c0fa91db0e7d06fc4a
Mahbub <hkhan.cse@gmail.com>
2024-10-03 12:12:50 +0300

    Updated code for static and dynamic content for blog/page.tsx file

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,12 +1,19 @@
-function sleep() {
-  return new Promise((resolve) => setTimeout(resolve, 5000))
-}
+// function sleep() {
+//   return new Promise((resolve) => setTimeout(resolve, 5000))
+// }
 
 const blog = async () => {
-  await sleep()
+  // await sleep()
+
+  const blogs = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    cache: "no-store",
+  })
+  const response = await blogs.json()
+  console.log(response)
+
   return (
     <div>
-      <h1>Blog</h1>
+      <h1>Blog 2 {new Date().toLocaleTimeString()}</h1>
     </div>
   )
 }
